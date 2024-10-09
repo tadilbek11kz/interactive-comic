7ce860e4c4ce8a2c379706f756b4f43755ffc69b
tadilbek11kz <tadilbek11kz@gmail.com>
2024-10-09 07:48:49 +0400

    feat: add mute button

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -1,6 +1,7 @@
 
 // Get references to the toggle and container elements
 const toggle = document.getElementById('mode-toggle');
+const audioToggle = document.getElementById('audio-toggle');
 const container = document.getElementById('main-page');
 const moral = document.getElementById('moral');
 
@@ -186,4 +187,20 @@ toggle.addEventListener('click', (e) => {
 moral.addEventListener('click', (e) => {
     e.preventDefault();
     window.location.href = "moral.html";
+});
+
+audioToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    let muted = null;
+    const audio = document.querySelectorAll('audio').forEach(audio => {
+        muted = audio.muted;
+        audio.muted = !audio.muted;
+    });
+    if (muted) {
+        audioToggle.innerHTML = '<path d="M12.5493 4.50005C11.3193 4.04005 8.70926 5.49996 6.54926 7.40996H4.94922C3.88835 7.40996 2.87093 7.83145 2.12079 8.58159C1.37064 9.33174 0.949219 10.3491 0.949219 11.41V13.41C0.949219 14.4708 1.37064 15.4883 2.12079 16.2385C2.87093 16.9886 3.88835 17.41 4.94922 17.41H6.54926C8.65926 19.35 11.2693 20.78 12.5493 20.33C14.6493 19.55 14.9992 15.33 14.9992 12.41C14.9992 9.48996 14.6493 5.28005 12.5493 4.50005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M20.6602 6.71997C22.1593 8.22011 23.0015 10.2542 23.0015 12.375C23.0015 14.4958 22.1593 16.5299 20.6602 18.03" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M18.5391 15.95C19.4764 15.0123 20.003 13.7407 20.003 12.4149C20.003 11.0891 19.4764 9.81764 18.5391 8.88" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>';
+        audio.muted = false;
+    } else {
+        audioToggle.innerHTML = '<path d="M12.5493 4.50005C11.3193 4.04005 8.70926 5.49996 6.54926 7.40996H4.94922C3.88835 7.40996 2.87093 7.83145 2.12079 8.58159C1.37064 9.33174 0.949219 10.3491 0.949219 11.41V13.41C0.949219 14.4708 1.37064 15.4883 2.12079 16.2385C2.87093 16.9886 3.88835 17.41 4.94922 17.41H6.54926C8.65926 19.35 11.2693 20.78 12.5493 20.33C14.6493 19.55 14.9992 15.33 14.9992 12.41C14.9992 9.48996 14.6493 5.28005 12.5493 4.50005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />'
+        audio.muted = true;
+    }
 });
