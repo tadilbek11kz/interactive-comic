ddfb15eb30e8765e3628df20d287993ca1256e09
tadilbek11kz <tadilbek11kz@gmail.com>
2024-10-09 06:03:04 +0400

    feat: add images

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -11,32 +11,36 @@ const images = [
     { src: "assets/img/frame_1.jpg", alt: "Frame 1" },
     { src: "assets/img/frame_2.jpg", alt: "Frame 2" },
     { src: "assets/img/frame_3.jpg", alt: "Frame 3" },
-    { src: "assets/img/torch.png", alt: "Mathces" },
-    { src: "assets/img/matches.png", alt: "Torch" },
-];
 
-if (selectedObject === "matches") {
-    images.push(
-        { src: "assets/img/frame_4.1.jpg", alt: "Frame 4" },
-        { src: "assets/img/frame_5.1.jpg", alt: "Frame 5" },
-        { src: "assets/img/frame_6.1.jpg", alt: "Frame 6" },
-        { src: "assets/img/frame_7.1.jpg", alt: "Frame 7" },
-    );
-} else {
-    images.push(
-        { src: "assets/img/frame_4.2.jpg", alt: "Frame 4" },
-        { src: "assets/img/frame_5.2.jpg", alt: "Frame 5" },
-        { src: "assets/img/frame_6.2.jpg", alt: "Frame 6" },
-        { src: "assets/img/frame_7.2.jpg", alt: "Frame 7" },
-    );
-}
+    { src: "assets/img/frame_4.1.jpg", alt: "Frame 4", id: "matches-frame" },
+    { src: "assets/img/frame_5.1.jpg", alt: "Frame 5", id: "matches-frame" },
+    { src: "assets/img/frame_6.1.jpg", alt: "Frame 6", id: "matches-frame" },
+    { src: "assets/img/frame_7.1.jpg", alt: "Frame 7", id: "matches-frame" },
 
-images.push(
+    { src: "assets/img/frame_4.2.jpg", alt: "Frame 4", id: "torch-frame" },
+    { src: "assets/img/frame_5.2.jpg", alt: "Frame 5", id: "torch-frame" },
+    { src: "assets/img/frame_6.2.jpg", alt: "Frame 6", id: "torch-frame" },
+    { src: "assets/img/frame_7.2.jpg", alt: "Frame 7", id: "torch-frame" },
+
     { src: "assets/img/frame_8.jpg", alt: "Frame 8" },
     { src: "assets/img/frame_9.jpg", alt: "Frame 9" },
     { src: "assets/img/frame_10.jpg", alt: "Frame 10" },
     { src: "assets/img/frame_11.jpg", alt: "Frame 11" },
     { src: "assets/img/frame_12.jpg", alt: "Frame 12" },
+];
+
+if (selectedObject === "matches") {
+    images.push(
+
+    );
+} else {
+    images.push(
+
+    );
+}
+
+images.push(
+
 )
 
 
@@ -46,19 +50,68 @@ let currentIndex = 0;
 
 function loadImages() {
     images.forEach((image, index) => {
+        if (index === 2) {
+            const div = document.createElement('div');
+            div.className = 'selection';
+            const img = document.createElement('img');
+            img.src = image.src;
+            img.alt = image.alt;
+            img.className = 'carousel-image';
+            const torch = document.createElement('img');
+            torch.addEventListener('click', (e) => {
+                e.preventDefault();
+                const matchesImg = document.querySelectorAll('.matches-frame');
+                matchesImg.forEach(img => {
+                    img.className = '';
+                });
+                nextBtn.style.display = 'block';
+            });
+            torch.src = "assets/img/torch-2.png";
+            torch.alt = "Torch";
+            torch.className = 'overlay torch';
+            const matches = document.createElement('img');
+            matches.addEventListener('click', (e) => {
+                e.preventDefault();
+                const torchImg = document.querySelectorAll('.torch-frame');
+                torchImg.forEach(img => {
+                    img.className = '';
+                });
+                nextBtn.style.display = 'block';
+            });
+            matches.src = "assets/img/matches-2.png";
+            matches.alt = "Matches";
+            matches.className = 'overlay matches';
+            div.appendChild(img);
+            div.appendChild(torch);
+            div.appendChild(matches);
+            carouselImages.appendChild(div);
+            return;
+        }
+
         const img = document.createElement('img');
         img.src = image.src;
         img.alt = image.alt;
+        img.id = image.id;
         img.className = index === 0 ? 'carousel-image active' : 'carousel-image';
         carouselImages.appendChild(img);
     });
+    nextBtn.style.display = currentIndex === images.length - 1 ? 'none' : 'block';
+    prevBtn.style.display = currentIndex === 0 ? 'none' : 'block';
 }
 
 function showImage(index) {
-    const imgs = carouselImages.querySelectorAll('img');
+    const imgs = carouselImages.querySelectorAll('img.carousel-image');
     imgs[currentIndex].classList.remove('active');
     imgs[index].classList.add('active');
     currentIndex = index;
+
+    nextBtn.style.display = currentIndex === images.length - 1 || currentIndex === 2 ? 'none' : 'block';
+    prevBtn.style.display = currentIndex === 0 ? 'none' : 'block';
+
+    const overlays = document.querySelectorAll('.overlay');
+    overlays.forEach(overlay => {
+        overlay.style.display = index === 2 ? 'block' : 'none';
+    });
 }
 
 function nextImage() {
